60c6a6dc35cb5675d3e21b3d2f01f0b88816da68
Jorge García (Bardok) <bardok@gmail.com>
2015-01-11 18:06:57 +0100

    Grunt project. Example file. Initial data option.

---
+++ Gruntfile.js
@@ -0,0 +1,20 @@
+module.exports = function(grunt){
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+	uglify: {
+	  options: {
+	    mangle: true
+	  },
+	  my_target: {
+	    files: {
+	      'dist/networksim.min.js': ['js/*.js']
+	    }
+	  }
+	}
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.registerTask('default', ['uglify']);
+
+};
